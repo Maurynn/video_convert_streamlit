9a54c20f8780b46d5598de4749db826b381c8927
Mauro Alves <mauro.mn@hotmail.com>
2023-06-26 17:17:34 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -23,26 +23,22 @@ if st.sidebar.checkbox('Mostrar instruções de uso'):
     5. **Download de vídeo:** Se você inseriu um link do YouTube, também poderá baixar o vídeo original clicando no botão "Download vídeo".
     
     Se você tiver qualquer dúvida ou problema, entre em contato com o suporte.
+Email: mauro.mn@hotmail.com
     """)
 
 video_file = st.file_uploader("Carregue um arquivo de vídeo", type=['mp4', 'mov', 'avi', 'flv', 'wmv'])
 
 youtube_link = st.text_input('Ou, insira um link do YouTube e aperte Enter.')
 
-def download_youtube_video(url, is_playlist=False):
+def download_youtube_video(url):
     try:
         ydl_opts = {'outtmpl': 'downloaded_videos/%(title)s.%(ext)s'}
-        if is_playlist:
-            ydl_opts['noplaylist'] = False
         with YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
-            if is_playlist:
-                return [entry['webpage_url'] for entry in info_dict['entries']]
-            else:
-                video_url = ydl.prepare_filename(info_dict)
-                return video_url
+            video_url = ydl.prepare_filename(info_dict)
+            return video_url
     except DownloadError:
-        st.error("Erro ao baixar o vídeo do YouTube. Por favor, verifique o link e tente novamente.")
+        st.error("Ocorreu um erro ao tentar baixar o vídeo. Por favor, verifique o link e tente novamente ou insira um novo link..")
         return None
 
 if video_file is not None:
@@ -61,26 +57,24 @@ if video_file is not None:
             st.download_button(label="Download MP3", data=audio_data, file_name="output.mp3", mime="audio/mpeg")
 
 if youtube_link:
-    is_playlist = st.checkbox("É uma playlist?")
-    video_file_paths = download_youtube_video(youtube_link, is_playlist)
-    if video_file_paths is not None:
-        for video_file_path in video_file_paths:
-            video_clip = VideoFileClip(video_file_path)
-            st.video(video_file_path)
+    video_file_path = download_youtube_video(youtube_link)
+    if video_file_path is not None:
+        video_clip = VideoFileClip(video_file_path)
+        st.video(video_file_path)
 
-            if st.button(f'Converter YouTube para MP3 - {video_file_path}'):
-                with st.spinner('Convertendo vídeo do YouTube...'):
-                    audio_clip = video_clip.audio
-                    audio_file = f"{video_file_path}.mp3"
-                    audio_clip.write_audiofile(audio_file)
-                    audio_data = open(audio_file, 'rb').read()
-                    st.audio(audio_data, format='audio/mp3')
-                    st.download_button(label="Download YouTube MP3", data=audio_data, file_name="youtube_output.mp3", mime="audio/mpeg")
+        if st.button('Converter YouTube para MP3'):
+            with st.spinner('Convertendo vídeo do YouTube...'):
+                audio_clip = video_clip.audio
+                audio_file = f"{video_file_path}.mp3"
+                audio_clip.write_audiofile(audio_file)
+                audio_data = open(audio_file, 'rb').read()
+                st.audio(audio_data, format='audio/mp3')
+                st.download_button(label="Download YouTube MP3", data=audio_data, file_name="youtube_output.mp3", mime="audio/mpeg")
 
-            if st.button(f'Download YouTube Video - {video_file_path}'):
-                with st.spinner('Preparando vídeo do YouTube para download...'):
-                    sleep(3)
-                    video_data = open(video_file_path, 'rb').read()
-                    
-                    st.info('Vídeo pronto para download, Clique no botão abaixo:')
-                    st.download_button(label="Clique aqui para baixar o vídeo ", data=video_data, file_name="youtube_video.mp4", mime="video/mp4")
+        if st.button('Download YouTube Video'):
+            with st.spinner('Preparando vídeo do YouTube para download...'):
+                sleep(3)
+                video_data = open(video_file_path, 'rb').read()
+                
+                st.info('Vídeo pronto para download, Clique no botão abaixo:')
+                st.download_button(label="Clique aqui para baixar o vídeo ", data=video_data, file_name="youtube_video.mp4", mime="video/mp4")
