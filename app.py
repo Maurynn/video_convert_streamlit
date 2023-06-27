8bf165e218e224fed78b0cf78fd2ce639ea2eed0
Mauro Alves <mauro.mn@hotmail.com>
2023-06-27 17:03:26 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -42,8 +42,6 @@ Email: mauro.mn@hotmail.com
 
 
 st.markdown('')
-st.markdown('')
-st.markdown('')
 
 
 video_file = st.file_uploader("Carregue um arquivo de vídeo", type=['mp4', 'mov', 'avi', 'flv', 'wmv'])
