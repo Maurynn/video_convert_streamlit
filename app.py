525a56e0474858faa4f496da4dbacea63286582b
Mauro Alves <mauro.mn@hotmail.com>
2023-06-27 22:05:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,21 @@ st.markdown("""
 
 st.markdown('<h1 class="big-font">📽️ VIDEO CONVERTER ONLINE 🎶</h1>', unsafe_allow_html=True)
 st.divider()
-
+if st.sidebar.checkbox('Mostrar instruções de uso'):
+    st.sidebar.write("""
+    1. **Carregar um arquivo de vídeo:** Clique no botão "Carregar um arquivo de vídeo" e escolha um arquivo de vídeo do seu dispositivo em um dos formatos suportados (.mp4, .mov, .avi, .flv, .wmv).
+    
+    2. **Inserir um link do YouTube:** Se você preferir, pode simplesmente inserir o link do vídeo do YouTube no campo de entrada "Insira um link do YouTube".
+    
+    3. **Converter para MP3:** Após carregar o arquivo de vídeo ou inserir o link do YouTube, clique no botão "Converter para MP3". O vídeo será convertido em um arquivo de áudio MP3.
+    
+    4. **Download de MP3:** Após a conversão, um botão "Download MP3" aparecerá. Clique nele para baixar o arquivo de áudio MP3 para o seu dispositivo.
+    
+    5. **Download de vídeo:** Se você inseriu um link do YouTube, também poderá baixar o vídeo original clicando no botão "Download vídeo".
+    
+    Se você tiver qualquer dúvida ou problema, entre em contato com o suporte.
+Email: mauro.mn@hotmail.com
+    """)
 
 youtube_link = st.text_input('Insira um link do YouTube e aperte Enter:', help="Clique em compartilhar no vídeo do YouTube e copie o link que será colado aqui... Também funciona com vídeos do Facebook.")
 
