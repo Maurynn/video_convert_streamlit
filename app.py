105601c72cf3e369e07978b90b07bb7d1e30487a
Mauro Alves <mauro.mn@hotmail.com>
2023-06-28 20:39:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with tab1:
     st.markdown('<h1 class="big-font">📽️ VIDEO CONVERTER ONLINE 🎶</h1>', unsafe_allow_html=True)
     st.divider()
     
-    youtube_link = st.text_input('Insira um link do YouTube e aperte Enter:', help="Clique em compartilhar no vídeo do YouTube e copie o link que será colado aqui... Também funciona com vídeos do Facebook.")
+    youtube_link = st.text_input('Insira um link do YouTube e aperte Enter:', placeholder="Cole o link aqui.", help="Clique em compartilhar no vídeo do YouTube e copie o link que será colado aqui... Também funciona com vídeos do Facebook.")
 
     video_file = st.file_uploader("Carregue um arquivo de vídeo:", type=['mp4', 'mov', 'avi', 'flv', 'wmv'],help="Selecione o arquivo de vídeo que você deseja converter para áudio.")
 
